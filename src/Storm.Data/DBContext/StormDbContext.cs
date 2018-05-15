48df075644535beb38d2871eee86981a3bb041f0
gongtao <1056685839@qq.com>
2018-05-15 17:44:16 +0800

    整理优化

--- src/Storm.Data/DBContext/StormDbContext.cs
+++ src/Storm.Data/DBContext/StormDbContext.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 
 namespace Storm.Data
 {
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
     public class StormDbContext : DbContext
     {
         public StormDbContext()
--- src/Storm.Web/App_Start/RouteConfig.cs
+++ src/Storm.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Storm.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
